8c629f360d5e08aaa1197a91d339585badcee57a
ciaba17 <gabrycdp@gmail.com>
2025-03-18 13:59:26 +0100

    risolto merge

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
 #include <SFML/Graphics.hpp>
 
 //ciao
+using namespace std;
+
+
+
+sf::RenderWindow window(sf::VideoMode(1920, 1080), "gioco dei fantini"); // Crea l'oggetto finestra
+sf::Event event; // Crea gestore event
+
+int main() {
+
+    while (window.isOpen()) {
+
+        while (window.pollEvent(event)) {
+            
+        }
+    }
+}
