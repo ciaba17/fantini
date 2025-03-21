b1f048ab9562b5623114eb358d5d50926e951927
ciaba17 <gabrycdp@gmail.com>
2025-03-21 15:45:39 +0100

    aggiunto titolo menu

--- main.cpp
+++ main.cpp
@@ -1,12 +1,17 @@
-#include <time.h>
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <time.h>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+const float WIDTH = 1920;
+const float HEIGHT = 1080;
+
 sf::RenderWindow window(sf::VideoMode(1920, 1080), "gioco dei fantini"); // Crea l'oggetto finestra
 sf::Event event; // Crea gestore event
+sf::Font font; 
 
 struct Player {
     string nome;
@@ -39,17 +44,20 @@ struct Bottone {
         area = sf::FloatRect(x, y, width, height);
 
         
-        shape.setPosition(sf::Vector2f(x, y));
         shape.setSize(sf::Vector2f(width, height));
         shape.setFillColor(colore);
+        shape.setOrigin(shape.getSize().x/2, shape.getSize().y/2);
+        shape.setPosition(sf::Vector2f(x, y));
     }
 
-    void drawBottone() {
+    void draw() {
         window.draw(shape);
     }
 
 };
 
+void draw();
+void drawTitolo();
 
 bool suTasto();
 
@@ -60,14 +68,55 @@ vector<Bottone> bottoni;
 
 int main() {
     srand(time(NULL));
-
-    //--- LOOP PRINCIPALE ---
-    while (window.isOpen()) {
-
-        while (window.pollEvent(event)) {
-            
-        }
+    // Carica il font per i testi
+    if (!font.loadFromFile("data/arial.ttf")) {
+        std::cerr << "Errore: impossibile caricare il font!" << std::endl;
+        return -1;
     }
+
+
+    //--- LOOP PRINCIPALE DEL GIOCO ---
+    while (window.isOpen()) {
+        
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+
+        
+
+        draw();
+
+    }
+}
+
+
+void draw() {
+    window.clear();
+    drawTitolo();
+
+
+    window.display();
+}
+
+void drawTitolo() {
+    sf::RectangleShape shape;
+    shape.setSize(sf::Vector2f(WIDTH/1.8, HEIGHT/1.8));
+    shape.setOrigin(shape.getSize().x/2, shape.getSize().y/2);
+    shape.setPosition(sf::Vector2f(WIDTH/2, HEIGHT/2));
+
+    sf::Text testo;
+    testo.setFont(font);
+    testo.setString("BENVENUTO AL GIOCO \n           DELL'OCA");
+    testo.setCharacterSize(80);
+    testo.setFillColor(sf::Color::Black);
+    sf::FloatRect textBounds = testo.getLocalBounds();
+    testo.setOrigin(textBounds.width/2, textBounds.height/2);
+    testo.setPosition(sf::Vector2f(WIDTH/2, HEIGHT/3));
+
+    window.draw(shape);
+    window.draw(testo);
 }
 
 bool suTasto() {
@@ -76,4 +125,5 @@ bool suTasto() {
             return true;
         }
     }
+    return false;
 }
