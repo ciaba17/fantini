61e7e878e057e50c88b1eed9959eb8d78719411e
ciaba17 <gabrycdp@gmail.com>
2025-03-22 10:15:46 +0100

    provando a fixare funzione suTasto

--- main.cpp
+++ main.cpp
@@ -12,6 +12,9 @@ const float HEIGHT = 1080;
 sf::RenderWindow window(sf::VideoMode(1920, 1080), "gioco dei fantini"); // Crea l'oggetto finestra
 sf::Event event; // Crea gestore event
 sf::Font font; 
+sf::Texture menuWPT;
+sf::Sprite menuWP;
+
 
 struct Player {
     string nome;
@@ -30,25 +33,39 @@ struct Bottone {
     bool premuto = false;
     float x, y;
     float width, height;
-    string testo;
     sf::Color colore;
     sf::RectangleShape shape;
+    sf::Text testo;
     sf::FloatRect area;
     
-    Bottone(float x, float y, float width, float height, string testo, sf::Color colore) {
-        this -> x = x;
-        this -> y = y;
-        this -> testo = testo;
-        this -> width = width;
-        this -> height = height;
+    
+    Bottone(float x, float y, float width, float height, string testoS, sf::Color colore) {
+        this->x = x;
+        this->y = y;
+        this->width = width;
+        this->height = height;
+    
         area = sf::FloatRect(x, y, width, height);
-
-
+    
         shape.setSize(sf::Vector2f(width, height));
         shape.setOutlineColor(sf::Color::Black);
         shape.setFillColor(colore);
-        shape.setOrigin(shape.getSize().x/2, shape.getSize().y/2);
+        shape.setOrigin(shape.getSize().x / 2, shape.getSize().y / 2);
         shape.setPosition(sf::Vector2f(x, y));
+    
+        testo.setFont(font);
+        testo.setString(testoS);
+        testo.setCharacterSize(WIDTH / 18);
+        testo.setFillColor(sf::Color::Black);
+        sf::FloatRect textBounds = testo.getLocalBounds();
+        testo.setOrigin(textBounds.width / 2, textBounds.height / 2);
+        testo.setPosition(sf::Vector2f(x, y));  // Posiziona il testo al centro
+    }
+    
+
+    void draw() {
+        window.draw(shape);
+        window.draw(testo);
     }
 };
 
@@ -64,16 +81,20 @@ int tiraDadi(int nDadi,int faccieDado);
 
 vector<Player> players;
 vector<Bottone> bottoni;
+
+
 bool menu = true;
 
 
 int main() {
     srand(time(NULL));
     // Carica il font per i testi
-    if (!font.loadFromFile("data/arial.ttf")) {
-        std::cerr << "Errore: impossibile caricare il font!" << std::endl;
-        return -1;
-    }
+    font.loadFromFile("data/arial.ttf");
+    menuWPT.loadFromFile("data/menuWP.jpg");
+    menuWP.setTexture(menuWPT);
+    menuWP.setPosition(0,0);
+    sf::Vector2u textureSize = menuWPT.getSize();
+    menuWP.setScale(WIDTH / textureSize.x, HEIGHT / textureSize.y);
 
     bottoni.push_back(Bottone(WIDTH/2, HEIGHT/1.5, WIDTH*0.2, HEIGHT*0.2, "START", sf::Color::Red));
 
@@ -88,7 +109,6 @@ int main() {
         }
         
         update();
-        
 
         draw();
 
@@ -100,7 +120,7 @@ void input() {
     if (event.type == sf::Event::KeyPressed) {
     }
 
-    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && suBottone) {
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Left) && suBottone()) {
         if (menu) {
             menu = false;
             bottoni.erase(bottoni.begin());
@@ -111,10 +131,9 @@ void input() {
 
 
 void update() {
-    if (menu) {
-
-    }
+    cerr << sf::Mouse::getPosition(window).x << "\t";
 }
+
 
 void draw() {
     window.clear();
@@ -137,13 +156,14 @@ void drawMenu() {
 
     sf::Text testo;
     testo.setFont(font);
-    testo.setString("BENVENUTO AL GIOCO \n           DELL'OCA");
-    testo.setCharacterSize(80);
+    testo.setString(" BENVENUTO AL GIOCO \n           DELL'OCA");
+    testo.setCharacterSize(WIDTH/22);
     testo.setFillColor(sf::Color::Black);
     sf::FloatRect textBounds = testo.getLocalBounds();
     testo.setOrigin(textBounds.width/2, textBounds.height/2);
     testo.setPosition(sf::Vector2f(WIDTH/2, HEIGHT/3));
 
+    window.draw(menuWP);
     window.draw(shape);
     window.draw(testo);
 }
@@ -151,24 +171,28 @@ void drawMenu() {
 
 void drawBottoni() {
     for (auto bottone : bottoni) {
-        window.draw(bottone.shape);
+        bottone.draw();
     }
 }
 
 
 bool suBottone() {
-    for (auto bottone : bottoni) { // Controlla tutti i bottoni
-        if (bottone.area.contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(window)))) { // Vede se il mouse si trova all'interno di un bottone
+    for (auto &bottone : bottoni) { // Controlla tutti i bottoni
+        if (sf::Mouse::getPosition(window).x > bottone.x &&
+            sf::Mouse::getPosition(window).x < bottone.x + bottone.width &&
+            sf::Mouse::getPosition(window).y > bottone.y &&
+            sf::Mouse::getPosition(window).y < bottone.y + bottone.height) { // Vede se il mouse si trova all'interno di un bottone
             return true;
         }
     }
+
     return false;
 }
 
 
 int tiraDadi(int nDadi,int faccieDado){
     
-    int totale=0;
+    int totale = 0;
 
     //genera e somma i numeri casuali dei dadi
     for(int i=0;i<nDadi;i++){
